9a608def98e6f3235d72c00fdce511d412b169ab
Deborah Sanchez <deborah@quarksalchemist.com>
2024-01-02 22:49:40 +0800

    devlink into github repository

---
+++ bit-and-rock/.webflowrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: "https://api.webflow.com",
+  rootDir: "./devlink",
+  siteId: "659017a464761a9cccff3260",
+  authToken: "8f926ac78e00f7d8d37bbb19d927269e658db152bc6574afc8215f973ad50610", // An environment variable is recommended for this field.
+  cssModules: true,
+};
---
+++ bit-and-rock/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { BitAndRock } from "../../devlink";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <BitAndRock />
+      </div>
+    </main>
+  );
+}
